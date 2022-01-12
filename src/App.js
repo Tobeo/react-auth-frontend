f979208d56eef73cff97c8ff5c4b688a3d6b025f
Tobias Nordvall <tobeono1@gmail.com>
2022-01-12 20:33:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { Card, Tab, Tabs } from "@blueprintjs/core";
+import { useCallback, useContext, useEffect, useState } from "react";
+import { UserContext } from "./context/UserContext";
+import Loader from "./Loader";
+import Login from "./Login";
+import Register from "./Register";
+import Welcome from "./Welcome";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [currentTab, setCurrentTab] = useState("login");
+  const [userContext, setUserContext] = useContext(UserContext);
+
+  const verifyUser = useCallback(() => {
+    fetch(process.env.REACT_APP_API_ENDPOINT + "users/refreshToken", {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+    }).then(async (response) => {
+      if (response.ok) {
+        const data = await response.json();
+        setUserContext((oldValues) => {
+          return { ...oldValues, token: data.token };
+        });
+      } else {
+        setUserContext((oldValues) => {
+          return { ...oldValues, token: null };
+        });
+      }
+      // call refreshToken every 5 minutes to renew the authentication token.
+      setTimeout(verifyUser, 5 * 60 * 1000);
+    });
+  }, [setUserContext]);
+
+  useEffect(() => {
+    verifyUser();
+  }, [verifyUser]);
+
+  /**
+   * Sync logout across tabs
+   */
+  const syncLogout = useCallback((event) => {
+    if (event.key === "logout") {
+      // If using react-router-dom, you may call history.push("/")
+      window.location.reload();
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("storage", syncLogout);
+    return () => {
+      window.removeEventListener("storage", syncLogout);
+    };
+  }, [syncLogout]);
+
+  return userContext.token === null ? (
+    <Card elevation="1">
+      <Tabs id="Tabs" onChange={setCurrentTab} selectedTabId={currentTab}>
+        <Tab id="login" title="Login" panel={<Login />} />
+        <Tab id="register" title="Register" panel={<Register />} />
+        <Tabs.Expander />
+      </Tabs>
+    </Card>
+  ) : userContext.token ? (
+    <Welcome />
+  ) : (
+    <Loader />
   );
 }
 
